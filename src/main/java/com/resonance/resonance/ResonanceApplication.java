3994ab2c2491b80935d82f368e7c55358d05fad5
Ayush Jha <ayushjha292005@gmail.com>
2026-07-23 20:41:22 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/resonance/resonance/ResonanceApplication.java
@@ -0,0 +1,13 @@
+package com.resonance.resonance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResonanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResonanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resonance/resonance/ResonanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resonance.resonance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResonanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
